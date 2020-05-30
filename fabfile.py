db06aaea3bad856bcfdc521e3c1f0d41b5eb8f47
KartikeyParashar <parasharkartikey@gmail.com>
2020-05-30 19:11:12 +0530

    updated the fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,40 @@
 from fabric.api import local
 
 
-def runserver():
-    local("python3 manage.py runserver")
+def backup():
+    local("git pull")
+    local("git add .")
+    print("Enter Your Commit Comment: ")
+    comment = raw_input()
+    local("git commit -m '%s'" % comment)
+    local("git push")
 
 
-def fuser():
-    local("fuser -k 8000/tcp")
+# Before using this function, use this command -> export DJANGO_SETTINGS_MODULE=FUNDOONOTES.settings
+def celeryworker():
+    local("celery -A FUNDOONOTES worker -l info")
+
+
+# Before using this function, use this command -> export DJANGO_SETTINGS_MODULE=FUNDOONOTES.settings
+def celerybeat():
+    local("celery -A FUNDOONOTES beat -l info")
 
 
 def elasticsearch():
     local("python3 manage.py search_index --rebuild")
 
 
-def backup():
-    local("git pull")
-    local("git add .")
-    comment = input("Enter Your Commit Comment: ")
-    local("git commit -m '%s'" % comment)
-    local("git push")
+def fuser():
+    local("fuser -k 8000/tcp")
+
+
+def makemigrations():
+    local("python3 manage.py makemigrations")
+
+
+def migrate():
+    local("python3 manage.py migrate")
+
+
+def runserver():
+    local("python3 manage.py runserver")
